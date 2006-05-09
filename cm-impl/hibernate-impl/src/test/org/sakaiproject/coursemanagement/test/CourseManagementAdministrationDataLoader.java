e900323dbc9e8bfaac046e5c35225e4085157974
jholtzman@berkeley.edu <jholtzman@berkeley.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-05-09 16:38:32 +0000

    CM-31 -- Added sample data to dataloader, and updated sample JDBC connection to use sakai's DB.
    git-svn-id: https://source.sakaiproject.org/svn/course-management/trunk@9169 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- cm-impl/hibernate-impl/src/test/org/sakaiproject/coursemanagement/test/CourseManagementAdministrationDataLoader.java
+++ cm-impl/hibernate-impl/src/test/org/sakaiproject/coursemanagement/test/CourseManagementAdministrationDataLoader.java
@@ -21,11 +21,11 @@
  **********************************************************************************/
 package org.sakaiproject.coursemanagement.test;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 
-import org.sakaiproject.coursemanagement.api.AcademicSession;
 import org.sakaiproject.coursemanagement.api.CourseManagementAdministration;
-import org.sakaiproject.coursemanagement.api.CourseManagementService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import junit.framework.TestCase;
@@ -41,19 +41,72 @@ import junit.framework.TestCase;
  */
 public class CourseManagementAdministrationDataLoader extends TestCase implements DataLoader {
 	private CourseManagementAdministration cmAdmin;
-	private CourseManagementService cmService;
 	public void testLoadData() throws Exception {
 		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(new String[] {"spring-test.xml", "spring-config-dataload.xml"});
 		cmAdmin = (CourseManagementAdministration)ac.getBean(CourseManagementAdministration.class.getName());
-		cmService = (CourseManagementService)ac.getBean(CourseManagementService.class.getName());
 		load();
 	}
 
 	public void load() throws Exception {
-		// Load your data here
-		cmAdmin.createAcademicSession("f2006", "Fall 2006", "The fall term, 2006", new Date(), new Date());
-		AcademicSession academicSession = cmService.getAcademicSession("f2006");
-		// etc
+		// Academic Sessions
+		Calendar startCal = Calendar.getInstance();
+		Calendar endCal = Calendar.getInstance();
+		
+		startCal.set(2006, 8, 1);
+		endCal.set(2006, 12, 1);
+		cmAdmin.createAcademicSession("f2006", "Fall 2006", "The fall term, 2006", startCal.getTime(), endCal.getTime());
+		
+		startCal.set(2007, 3, 1);
+		endCal.set(2007, 6, 1);
+		cmAdmin.createAcademicSession("sp2007", "Spring 2007", "The spring term, 2007", startCal.getTime(), endCal.getTime());
+		
+		cmAdmin.createAcademicSession("IND", "Independent Studies", "Ongoing session for independent study", null, null);
+		
+		// Canonical Courses
+		cmAdmin.createCanonicalCourse("bio101", "Intro to Biology", "An introduction to biology");
+		cmAdmin.createCanonicalCourse("indep_study_bio", "Independent Study in Biology", "A self paced independent study in biology.  Must be " +
+				"approved and sponsored by biology department faculty.");
+		
+		// Course Offerings
+		startCal.set(2006, 8, 1);
+		endCal.set(2006, 12, 1);
+		cmAdmin.createCourseOffering("bio101_f2006", "Bio 101, Fall 2006", "Intro to Biology, Fall 06", "f2006", "bio101", startCal.getTime(), endCal.getTime());
+
+		startCal.set(2007, 3, 1);
+		endCal.set(2007, 6, 1);
+		cmAdmin.createCourseOffering("bio101_sp2007", "Bio 101, Spring 2007", "Intro to Biology, Spring 07", "sp2007", "bio101", startCal.getTime(), endCal.getTime());
+		
+		cmAdmin.createCourseOffering("indep_study_bio_molecular_research", "Independent study in molecular research", "Details to be determined by student and sponsor",
+				"IND", "indep_study_bio", null, null);
+		
+		// Enrollment sets
+		Set instructors = new HashSet();
+		instructors.add("admin");
+		cmAdmin.createEnrollmentSet("bio101_f2006_lec1", "Bio 101 Lecture", "Bio 101 Lecture.  Required.", "lecture", "3", "bio101_f2006", instructors);
+		
+		instructors.clear();
+		instructors.add("ta1");
+		cmAdmin.createEnrollmentSet("bio101_f2006_lab1", "Lab 1", "Lab 1", "lab", "1", "bio101_f2006", instructors);
+
+		instructors.clear();
+		instructors.add("ta2");
+		cmAdmin.createEnrollmentSet("bio101_f2006_lab2", "Lab 2", "Lab 2", "lab", "1", "bio101_f2006", instructors);
+
+		// Enrollments
+		cmAdmin.addOrUpdateEnrollment("student1", "bio101_f2006_lec1", "enrolled", "3", "standard");
+		cmAdmin.addOrUpdateEnrollment("student2", "bio101_f2006_lec1", "enrolled", "3", "pass/fail");
+		cmAdmin.addOrUpdateEnrollment("student3", "bio101_f2006_lec1", "waitlisted", "3", "standard");
+		
+		cmAdmin.addOrUpdateEnrollment("student1", "bio101_f2006_lab1", "enrolled", "1", "standard");
+		cmAdmin.addOrUpdateEnrollment("student2", "bio101_f2006_lab1", "enrolled", "1", "pass/fail");
+
+		cmAdmin.addOrUpdateEnrollment("student3", "bio101_f2006_lab2", "waitlisted", "1", "standard");
+
+		// Sections
+		cmAdmin.createSection("bio101_f2006_lec1", "Bio 101, Lecture", "Intro to Biology, Fall 06, Lecture", "lecture", null, "bio101_f2006", "bio101_f2006_lec1");
+		cmAdmin.createSection("bio101_f2006_lab1", "Lab 1", "Intro to Biology, Fall 06, Lab 1", "lab", null, "bio101_f2006", "bio101_f2006_lab1");
+		cmAdmin.createSection("bio101_f2006_lab2", "Lab 2", "Intro to Biology, Fall 06, Lab 2", "lab", null, "bio101_f2006", "bio101_f2006_lab2");
+				
 	}
 
 }
